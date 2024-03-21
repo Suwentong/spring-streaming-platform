283c1000a446a600319fd5dfcf4517ad1fe45205
Suwentong <abdukadirov048@gmail.com>
2024-03-21 13:20:40 +0500

    initial commit

---
+++ src/test/java/com/company/streamingplatform/StreamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.streamingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

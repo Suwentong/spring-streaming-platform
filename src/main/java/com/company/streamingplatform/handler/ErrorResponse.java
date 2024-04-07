6b24097527937a9869c456414c75a41ec93da5e4
Suwentong <abdukadirov048@gmail.com>
2024-04-07 21:23:28 +0500

    add global exception handler

---
+++ src/main/java/com/company/streamingplatform/handler/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.company.streamingplatform.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    String message;
+}
---
+++ src/main/java/com/company/streamingplatform/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.company.streamingplatform.handler;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleException(IllegalStateException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleException(EntityNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(exception.getMessage()));
+    }
+}
